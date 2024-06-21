b9b94d96d7a08f421c97f5f7b2d6553d940a1c4f
AshaniImalsha <ashaniimalsha26@gmail.com>
2024-06-21 23:40:01 +0530

    updated skills assert

--- components/home/Skills.js
+++ components/home/Skills.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { skills } from "@/data/content/home";
+import { skills } from "@/data/cloudinary/image";
 
 function Skills() {
   return (
--- data/cloudinary/image.js
+++ data/cloudinary/image.js
@@ -1,5 +1,65 @@
 export const images = [
     {
-        userProfile : "https://res.cloudinary.com/dsqosc2ig/image/upload/v1713517546/portfolio/user%20profile.png"
+        userProfile: "https://res.cloudinary.com/dsqosc2ig/image/upload/v1713517546/portfolio/user%20profile.png"
     }
+]
+
+export const skills = [
+    {
+        title: "HTML",
+        icon: "https://res.cloudinary.com/dsqosc2ig/image/upload/v1702381164/skills-icon/html5_auahni.png",
+    },
+    {
+        title: "CSS",
+        icon: "https://res.cloudinary.com/dsqosc2ig/image/upload/v1702381149/skills-icon/css3_hoj6a0.png",
+    },
+    {
+        title: "Javascript",
+        icon: "https://res.cloudinary.com/dsqosc2ig/image/upload/v1702381167/skills-icon/javascript_jfndp4.png",
+    },
+    {
+        title: "Typescript",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+    },
+    {
+        title: "React",
+        icon: "https://res.cloudinary.com/dsqosc2ig/image/upload/v1702381246/skills-icon/react_hwngwl.png",
+    },
+    {
+        title: "NextJS",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg",
+        style: { filter: "invert(1)" },
+    },
+    {
+        title: "NodeJS",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+    },
+    {
+        title: "Java",
+        icon: "https://res.cloudinary.com/dsqosc2ig/image/upload/v1715351146/skills-icon/Java_q03qpm.png"
+    },
+    {
+        title: "SpringBoot",
+        icon: "https://res.cloudinary.com/dsqosc2ig/image/upload/v1715350824/skills-icon/SpringBoot_nn41j2.png",
+    },
+    {
+        title: "TailwindCSS",
+        icon: "https://res.cloudinary.com/dsqosc2ig/image/upload/v1715351000/skills-icon/Tailwind_CSS_logo.svg_gglm7a.png",
+    },
+    {
+        title: "Bootstrap",
+        icon: "https://res.cloudinary.com/dsqosc2ig/image/upload/v1702381142/skills-icon/bootstrap_jroje5.png"
+    },
+    {
+        title: "Git",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+    },
+    {
+        title: "Firebase",
+        icon: "https://res.cloudinary.com/dsqosc2ig/image/upload/v1702381155/skills-icon/firebase_ysbdb4.png",
+    },
+    {
+        title: "Figma",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
+    },
 ]

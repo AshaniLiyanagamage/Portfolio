7464c0d370fc9ad0f979cd1f328ecb3d5133c597
AshaniImalsha <ashaniimalsha26@gmail.com>
2024-06-21 23:57:40 +0530

    updated proflie

--- data/cloudinary/image.js
+++ data/cloudinary/image.js
@@ -1,6 +1,6 @@
 export const images = [
     {
-        userProfile: "https://res.cloudinary.com/dsqosc2ig/image/upload/v1713517546/portfolio/user%20profile.png"
+        userProfile: "https://res.cloudinary.com/dsqosc2ig/image/upload/v1718994405/portfolio/profile.png"
     }
 ]
 
